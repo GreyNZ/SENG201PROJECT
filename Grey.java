4e5f08933da97cccce65b66333e3d7ceca1456eb
GreyNZ <44332923+GreyNZ@users.noreply.github.com>
2019-04-15 18:35:43 +1200

    Add files via upload

---
+++ Grey.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Grey extends Person {
+	// example inputs (name, race, health, maxHealth, vigour, maxVigour, hunger, maxHunger, maxActions, currentActions, repairValue, searchValue, hasPlague
+	public Grey(String tempName, int unqiue) {
+		super(tempName, "Grey", 50, 50, 100, 100, 0, 100, 2, 2, 2.0, 1.5, false, unqiue);
+	}
+	
+	public static void main(String[] args) {
+		Person greyzilla = new Grey("Bighead of Woswell", 4);
+		System.out.println(greyzilla);
+		
+		
+		
+	}
+
+}
