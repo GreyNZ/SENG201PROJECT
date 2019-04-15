4db48be44fd113776729a736ae215bd7526fd236
Node-N <49627651+Node-N@users.noreply.github.com>
2019-04-15 18:25:01 +1200

    Add files via upload

--- Crew.java
+++ Crew.java
@@ -1,4 +1,71 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Crew {
+	
+	private ArrayList<Person> crewMemberArray;
+	private ArrayList<Item> foodArray;
+	private ArrayList<Item> medicalArray;
 
+	private int numberOfCrewMembers;
+	private Ship ship;
+	private Double money;
+	Scanner scanner = new Scanner(System.in);
+
+
+	
+	
+	//needs medical and food
+	public Crew(int numberOfCrewMembers, String shipName, Double money) {
+//		private ArrayList<Person> crewMemberArray = new ArrayList<Person>(numberOfCrewMembers);
+		this.crewMemberArray = new ArrayList<Person>(numberOfCrewMembers);
+		this.numberOfCrewMembers = numberOfCrewMembers;
+		this.money = money;
+		this.ship = new Ship(shipName);
+		
+		
+		//this will eventually be a switch statement inside the for loop
+
+		}
+	public void buildCrew() {
+		for (int i = 0; i <numberOfCrewMembers; i+=1) {
+			Person newMember = new Person();
+			this.crewMemberArray.add(newMember);
+			this.addCrewMember(i);
+		}
+		}
+	
+	public void addCrewMember(int i) {
+		System.out.println("Name? ");
+		String name = scanner.next();
+		System.out.println("Please select a race.\n1. Human\n2. Rockman\n3. Egghead\n4. Bugman\n5. Gazer\n6. Warbot");
+		try {
+			int raceNum = scanner.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("Not a valid selection. Defaulting to human");
+		}
+	}
+	
+	
+	
+	public void addMoney(Double cost) {
+		this.money -= cost;
+	}
+	public void removeMoney(Double cost) {
+		this.money += cost;
+	}
+	
+	public Double getMoney() {
+		return this.money;
+	}
+	
+	public static void main(String[] args) {
+		Crew crew = new Crew(4, "Betty", 2.0);
+		crew.buildCrew();
+	}
+	
 }
+
--- Ship.java
+++ Ship.java
@@ -1,4 +1,66 @@
+package main;
 
 public class Ship {
+	private String shipName;
+	private Double shipHealth = 100.0;
+	private Double shipShields = 100.0;
+	
+	public Ship(String name) {
+		this.shipName = name;
+	}
+	
+	public Double getShield() {
+		return this.shipShields;
+	}
+	public void raiseShield(Double raiseValue) {
+		if (this.shipShields + raiseValue > 100.0) {
+			this.shipShields = 100.0;
+		}
+		else {
+			this.shipShields += raiseValue;
+		}
+	}
+	public void lowerShield(Double lowerValue) {
+		if (this.shipShields - lowerValue <= 0.0) {
+			this.shipShields = 0.0;
+		}
+		else {
+			this.shipShields -= lowerValue;
+		}
+	}	
+	
+	//shields take full damage, hull takes damage * (1 - shield %)
+	public void takeDamage(Double damage) {
+		Double shieldModifier = 1 - (this.shipShields / 100);
+		this.shipHealth -=  damage * shieldModifier;
+		this.lowerShield(damage);
+		if (this.shipHealth <= 0.0) {
+			Game.gameOver();
+		}
+		
 
+	}
+	public String toString() {
+		String status = String.format("Shipname: %s\nShip Health: %s\nShip Shield: %s", this.shipName, this.shipHealth, this.shipShields);
+		return status;
+	}
+	
+	public static void main(String[] args) {
+		Ship newShip = new Ship("Betty");
+		newShip.takeDamage(20.0);
+		System.out.println(newShip);
+
+		newShip.takeDamage(20.0);
+		System.out.println(newShip);
+
+		newShip.takeDamage(20.0);
+		System.out.println(newShip);
+
+		newShip.takeDamage(20.0);
+		System.out.println(newShip);
+
+		newShip.takeDamage(900.0);
+
+		System.out.println(newShip);
+	}
 }
